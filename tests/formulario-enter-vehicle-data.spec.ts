97a640c60e6f5181f05ac967c40f2d08badc93d9
DieroKreator <qtechwired@gmail.com>
2024-10-01 18:04:18 -0300

    Test finished using Singleton design pattern

--- tests/formulario-enter-vehicle-data.spec.ts
+++ tests/formulario-enter-vehicle-data.spec.ts
@@ -30,15 +30,17 @@ test.describe('Enter Vehicle Data Form Tests', () => {
     await page.close()
   });
 
-  test('Preencher Enter vehicle Data Form', async ({ page }) => {
-    await page.locator('') //make title
+  test('Enviar Enter vehicle Data Form', async ({ page }) => {
+    const makeLabel = await page.locator('label.main', { hasText: 'Make' });
+    await expect(makeLabel).toBeVisible();
     await page.selectOption('#make', { value: 'BMW'});
     await page.selectOption('#model', { value: 'Motorcycle'});
     await page.fill('#cylindercapacity', '1000');
     await page.fill('#engineperformance', '200');
+    /* Improve */
     await page.fill('#dateofmanufacture', '10/01/2024');
-    await page.selectOption('#numberofseats', { value: 'Motorcycle'});
-    await page.check('input[id="righthanddriveyes"]');
+    await page.selectOption('#numberofseats', { value: '3'});
+    // await page.check('input[id="righthanddriveyes"]');
     await page.selectOption('#numberofseatsmotorcycle', { value: '3'});
     await page.selectOption('#fuel', { value: 'Diesel'});
     await page.fill('#payload', '197');
@@ -46,5 +48,23 @@ test.describe('Enter Vehicle Data Form Tests', () => {
     await page.fill('#listprice', '80000');
     await page.fill('#annualmileage', '20000');
     await page.click('#nextenterinsurantdata');
+
+    const firstNameLabel = await page.locator('label.main', { hasText: 'First Name' });
+    await expect(firstNameLabel).toBeVisible();
+    await page.fill('#firstname', 'Charlie');
+    await page.fill('#lastname', 'Kamp');
+    await page.fill('#birthdate', '10/01/2000');
+    await page.selectOption('#country', { value: 'Angola'});
+    await page.fill('#zipcode', '1252014');
+    await page.selectOption('#occupation', { value: 'Employee'});
+    const skydivingLabel = await page.locator('label:has-text(" Skydiving")');
+    await skydivingLabel.click();
+    // Validate that the checkbox is now checked
+    await expect(skydivingLabel).toBeChecked();
+
+    await page.click('#nextenterproductdata');
+
+    const startDateLabel = await page.locator('label.main', { hasText: 'Start Date' });
+    await expect(startDateLabel).toBeVisible();
   });
 });
