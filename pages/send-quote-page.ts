7506b2eab1e81643fce3f589d6883ac37313d421
DieroKreator <qtechwired@gmail.com>
2024-10-03 14:43:46 -0300

    Created all methods needed within the SendQuote page

--- pages/send-quote-page.ts
+++ pages/send-quote-page.ts
@@ -4,12 +4,20 @@ import { expect, Locator, Page } from "@playwright/test";
 export class SendQuotePage{
     readonly page: Page
     readonly makeLabel: Locator;
-    readonly modelDropdown: Locator;
+    readonly nextButton: Locator;
+    readonly firstNameLabel: Locator;
+    readonly skydivingLabel: Locator;
+    readonly nextProductButton: Locator;
+    readonly startDateLabel: Locator;
 
     constructor(page: Page) {
         this.page=page
         this.makeLabel = page.locator('label.main', { hasText: 'Make' });
-        // this.modelDropdown = page.locator('#make', { value: 'BMW' });
+        this.nextButton = page.locator('#nextenterinsurantdata');
+        this.firstNameLabel = page.locator('label.main');
+        this.skydivingLabel = page.locator('label:has-text("Skydiving")');
+        this.nextProductButton = page.locator('#nextenterproductdata');
+        this.startDateLabel = page.locator('label.main');
     }
 
     async validarTitle(title){
@@ -19,14 +27,97 @@ export class SendQuotePage{
     async validarMakeLabel(){
         await expect(this.makeLabel).toBeVisible();
     }
-  
-    // async selecionarMake(){
-    //     await this.modelDropdown.selectOption('#make', { value: 'BMW'});
-    //     await expect(this.quoteButton).toBeVisible();
-    //     await this.quoteButton.click();
-    // }
-    
-    // async selecionarModelo(){
-    //     await modelDropdown.selectOption('#make', { value: 'BMW'});
-    // }
+
+    // Metodo pra selecionar do dropdown
+    async selecionarMake(make: string) {
+        await this.page.selectOption('#make', { value: make });
+    }
+
+    async selecionarModel(model: string) {
+        await this.page.selectOption('#model', { value: model });
+    }
+
+    // Método pra preencher um campo
+    async preencherCylinderCapacity(cylinderCapacity: string) {
+        await this.page.fill('#cylindercapacity', cylinderCapacity);
+    }
+
+    async preencherEnginePerformance(enginePerformance: string) {
+        await this.page.fill('#engineperformance', enginePerformance);
+    }
+
+    async selecionarNumberofseats(numberofseats: string) {
+        await this.page.selectOption('#numberofseats', { value: numberofseats });
+    }
+
+    async selecionarNumberofseatsmotorcycle(numberofseatsmotorcycle: string) {
+        await this.page.selectOption('#numberofseatsmotorcycle', { value: numberofseatsmotorcycle });
+    }
+
+    async selecionarFuel(fuel: string) {
+        await this.page.selectOption('#fuel', { value: fuel });
+    }
+
+    async preencherPayload(payload: string) {
+        await this.page.fill('#payload', payload);
+    }
+
+    async preencherTotalweight(totalweight: string) {
+        await this.page.fill('#totalweight', totalweight);
+    }
+
+    async preencherListprice(listprice: string) {
+        await this.page.fill('#listprice', listprice);
+    }
+
+    async preencherAnnualmileage(annualmileage: string) {
+        await this.page.fill('#annualmileage', annualmileage);
+    }
+
+    async clickNextButton(){
+        await this.nextButton.click();
+    }
+
+    async validarFirstNameLabel(label: string){
+        await expect(this.firstNameLabel).toBeVisible();
+        await expect(this.firstNameLabel).toHaveText(label)
+    }
+
+    async preencherFirstName(firstname: string) {
+        await this.page.fill('#firstname', firstname);
+    }
+
+    async preencherLastName(lastname: string) {
+        await this.page.fill('#lastname', lastname);
+    }
+
+    async preencherBirthdate(birthdate: string) {
+        await this.page.fill('#birthdate', birthdate);
+    }
+
+    async selecionarCountry(country: string) {
+        await this.page.selectOption('#country', { value: country });
+    }
+
+    async preencherZipcode(zipcode: string) {
+        await this.page.fill('#zipcode', zipcode);
+    }
+
+    async selecionarOccupation(occupation: string) {
+        await this.page.selectOption('#occupation', { value: occupation });
+    }
+
+    async selecionarHobbies() {
+        await this.skydivingLabel.click();
+        await expect(this.skydivingLabel).toBeChecked();
+    }
+
+    async clickNextProductButton(){
+        await this.nextProductButton.click();
+    }
+
+    async validarStartDateLabel(label: string){
+        await expect(this.startDateLabel).toBeVisible();
+        await expect(this.startDateLabel).toHaveText(label)
+    }
 }
--- tests/formulario-enter-vehicle-data.spec.ts
+++ tests/formulario-enter-vehicle-data.spec.ts
@@ -6,6 +6,7 @@ test.describe('Enter Vehicle Data Form Tests', () => {
 
   test.beforeEach(async ({ page }) => {
     await page.goto('http://sampleapp.tricentis.com/101/app.php');
+    /* Validar a pagina abriu */
     await expect(page).toHaveTitle(/Vehicle Data/);
   });
 
@@ -14,57 +15,58 @@ test.describe('Enter Vehicle Data Form Tests', () => {
   });
 
   test('Enviar Enter vehicle Data Form', async ({ page }) => {
-    let homePage = new HomePage(page)
-    let sendQuotePagePage = new SendQuotePage(page)
+    let homePage = new HomePage(page);
+    let sendQuotePagePage = new SendQuotePage(page);
 
     // Validar titulo ao abrir a pagina Home
-    // homePage.validateTitle('Tricentis Vehicle Insurance')
-    // homePage.clickGetQuoteButton()
+    /* homePage.validateTitle('Tricentis Vehicle Insurance')
+    homePage.clickGetQuoteButton()*/
 
     // Validar titulo ao abrir a pagina SendQuote
-    sendQuotePagePage.validarTitle('Enter Vehicle Data')
+    sendQuotePagePage.validarTitle('Enter Vehicle Data');
     sendQuotePagePage.validarMakeLabel();
 
-    // Selecionar o make
-    await page.selectOption('#make', { value: 'BMW'});
-    // Selecionar um modelo
-    await page.selectOption('#model', { value: 'Motorcycle'});
+    sendQuotePagePage.selecionarMake('BMW');
+    sendQuotePagePage.selecionarModel('Motorcycle');
 
-    // Preencher campos Cylinder e Engine
-    await page.fill('#cylindercapacity', '1000');
-    await page.fill('#engineperformance', '200');
+    // Preencher campos 
+    await sendQuotePagePage.preencherCylinderCapacity('1000');
+    await sendQuotePagePage.preencherEnginePerformance('200');
 
     // Selecionar uma data
     /* Improve */
     await page.fill('#dateofmanufacture', '10/03/2024');
+
     //Selecionar o numero de cadeiras
-    await page.selectOption('#numberofseats', { value: '3'});
+    await sendQuotePagePage.selecionarNumberofseats('3');
 
     // await page.check('input[id="righthanddriveyes"]');
-    await page.selectOption('#numberofseatsmotorcycle', { value: '3'});
-    await page.selectOption('#fuel', { value: 'Diesel'});
-    await page.fill('#payload', '197');
-    await page.fill('#totalweight', '197');
-    await page.fill('#listprice', '80000');
-    await page.fill('#annualmileage', '20000');
-    await page.click('#nextenterinsurantdata');
+    await sendQuotePagePage.selecionarNumberofseatsmotorcycle('3');
+    await sendQuotePagePage.selecionarFuel('Diesel');
 
-    const firstNameLabel = await page.locator('label.main', { hasText: 'First Name' });
-    await expect(firstNameLabel).toBeVisible();
-    await page.fill('#firstname', 'Charlie');
-    await page.fill('#lastname', 'Kamp');
-    await page.fill('#birthdate', '10/01/2000');
-    await page.selectOption('#country', { value: 'Angola'});
-    await page.fill('#zipcode', '1252014');
-    await page.selectOption('#occupation', { value: 'Employee'});
-    const skydivingLabel = await page.locator('label:has-text(" Skydiving")');
-    await skydivingLabel.click();
-    // Validate that the checkbox is now checked
-    await expect(skydivingLabel).toBeChecked();
+    //Preencher campos
+    await sendQuotePagePage.preencherPayload('197');
+    await sendQuotePagePage.preencherTotalweight('197');
+    await sendQuotePagePage.preencherListprice('80000');
+    await sendQuotePagePage.preencherAnnualmileage('20000');
 
-    await page.click('#nextenterproductdata');
+    /* Click over Next button and navigate to next screen */
+    await sendQuotePagePage.clickNextButton();
+    /* Validar elemento na nova tela */
+    sendQuotePagePage.validarFirstNameLabel('First Name');
 
-    const startDateLabel = await page.locator('label.main', { hasText: 'Start Date' });
-    await expect(startDateLabel).toBeVisible();
+    sendQuotePagePage.preencherFirstName('Charlie');
+    sendQuotePagePage.preencherLastName('Kamp');
+    sendQuotePagePage.preencherBirthdate('10/01/2000');
+    sendQuotePagePage.selecionarCountry('Angola');
+    sendQuotePagePage.preencherZipcode('1252014');
+    sendQuotePagePage.selecionarOccupation('Employee');
+
+    /* Selecionar Hobbies com validação */
+    await sendQuotePagePage.selecionarHobbies();
+
+    /* Navegar a proxima tela do formulario com validação */
+    sendQuotePagePage.clickNextProductButton();
+    sendQuotePagePage.validarStartDateLabel('Start Date');
   });
 });
